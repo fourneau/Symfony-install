a00f781e272c4471eca116c047c161a1d64a1b73
fourneau <cathyjulien14@orange.fr>
2021-05-25 11:44:12 +0200

    cc

--- .history/src/Controller/DefaultController_20210525110811.php
+++ .history/src/Controller/DefaultController_20210525110811.php
@@ -19,6 +19,6 @@ class DefaultController extends AbstractController
      */
     public function index() {
 
-        return $this->render('default/index.html.twig', ['Bienvenue' => 'Bienvenue !']);
+        return $this->render('/index.html.twig', ['Bienvenue' => 'Bienvenue !']);
     }
     }
